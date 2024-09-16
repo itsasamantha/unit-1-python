2c146f7bc7f84a8989263b731dfdcac259e14631
sam <spowell@brooklynsteamcenter.org>
2024-09-16 10:48:31 -0400

    Benchmark Activity

---
+++ lesson_00.py
@@ -0,0 +1,40 @@
+# step 1 create lesson_00.py
+# step 2 This prints my name to the console
+print("Samanth P")
+
+# step 3 Creating a varible that stores my favorite show
+fav_show = "Arcane"
+
+# step 4 Creating a float
+my_float = 9.4
+
+# step 5 Creating a integer
+my_integer = 8
+
+# step 6 The prouduct of my_float and my_integer printed
+print(my_float * my_integer)
+
+# step 7 Favorite foods
+favorite_foods = ["Pepper Pot","Roti","Pumpkin Pie"]
+
+# step 8 Can get drivers license
+age = 17
+if age >= 16:
+    print("You can get a drivers license")
+else:
+    print("You are not old enough to get a drivers license")
+
+#step 9 Whats your name?
+users_name = input("What is your name? ")
+print(users_name)
+
+# step 10 for loop
+for i in favorite_foods:
+    print(i)
+
+# step 11 never running while loop
+num = 5
+while num == 4:
+    print("Soooooorrry")
+
+# Hurrayyyyy
