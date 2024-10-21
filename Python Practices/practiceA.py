5b04d977c6ea27f75f9419e3ce9903fd351c85ee
sam <spowell@brooklynsteamcenter.org>
2024-10-21 10:36:24 -0400

    practices a

---
+++ Python Practices/practiceA.py
@@ -0,0 +1,29 @@
+"""
+This is python practices a,a
+"""
+#imports the randint function from the random module
+from random import randint
+
+def higher_or_lower(num, val):
+    if num > val:
+        print("Your number is higher then the corect number")
+    if num < val:
+        print("Your number is lower than the correct number")
+
+answer = randint(0, 100)
+print("A number has been selected. Try to guess it!!")
+count = 0
+
+while count < 4 :
+    print(f"You have {4-count} attempts left")
+    guess = int(input("What is your guess "))
+    
+    if guess == answer:
+        print("You've guessed the number!!! Congraulations!!")
+        break
+    else:
+        higher_or_lower(guess, answer)
+        count += 1
+
+if count == 4:
+    print(f"You have run out of guesses. The number was {answer}")
